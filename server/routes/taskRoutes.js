1b3d3c9a221ff23cff09bbc4a03cd562a03cb0d4
Anshul887 <anshulmankar887@gmail.com>
2026-05-25 10:53:00 +0530

    Create taskRoutes.js

---
+++ server/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const auth = require("../middleware/auth");
+
+const {
+  getTasks,
+  createTask
+} = require("../controllers/taskController");
+
+router.get("/", auth, getTasks);
+router.post("/", auth, createTask);
+
+module.exports = router;
